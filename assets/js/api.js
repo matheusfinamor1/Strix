f00ba9706b28e6ea901303ccbbb56aaa1e3fa0d1
matheusfinamor1 <matheusfinamor1@gmail.com>
2024-01-09 15:07:47 -0300

    Adição das imagens no carousel obtidas da API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,15 +1,71 @@
+const API_KEY = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOGU0NDhjOWFjODQ5NTM2MjA0ZjViNTgyMjU0NjY3OCIsInN1YiI6IjY1OTQzMjFjYTU4OTAyNzExOTk3N2ZkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VyJ6swHmhFa0siWlfRYEnCPiMnm2QnEaicR_r8ZjrBw'
+const MOVIE_BASE_URL = 'https://api.themoviedb.org/3/movie/'
 
-async function fetchMovieList(){
-    const url = 'https://api.themoviedb.org/3/movie/changes?page=1';
+// Busca a lista de filme - SEM USO;
+async function fetchMovieList() {
+    const url = `${MOVIE_BASE_URL}/changes?page=1`;
 
     const options = {
         method: 'GET',
         headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOGU0NDhjOWFjODQ5NTM2MjA0ZjViNTgyMjU0NjY3OCIsInN1YiI6IjY1OTQzMjFjYTU4OTAyNzExOTk3N2ZkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VyJ6swHmhFa0siWlfRYEnCPiMnm2QnEaicR_r8ZjrBw'
+            Authorization: API_KEY
         }
     };
 
-    const fetching = await fetch(url,options)
-    return await fetching.json()
+    const fetchingMovieList = await fetch(url, options)
+    return await fetchingMovieList.json()
+}
+
+// Busca lista de filmes atualmente no cinema;
+// Retorna a lista de IDs desses filmes para fetchImagesMovie();
+async function fetchNowPlaying() {
+    const url = `${MOVIE_BASE_URL}/now_playing?language=pt-US&page=1`
+    let listIdMovie = []
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: API_KEY
+        }
+    };
+    const fetchingNowPlaying = await fetch(url, options)
+    const fetchingIdNowPlaying = await fetchingNowPlaying.json()
+
+    if (fetchingIdNowPlaying && fetchingIdNowPlaying.results) {
+
+        for (let i = 0; i < 3; i++) {
+            listIdMovie.push(fetchingIdNowPlaying.results[i].id)
+        }
+        return fetchPostersMovie(listIdMovie)
+
+    } else {
+        console.log("erro aos obter id");
+        return []
+    }
+}
+// Busca os Posters dos filmes no cinema atraves de seus IDs recebidos;
+async function fetchPostersMovie(listIdMovie) {
+    const postersPaths = []
+
+    for (const movieId of listIdMovie) {
+        const url = `${MOVIE_BASE_URL}${movieId}/images`
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: API_KEY
+            }
+        };
+        const fetchingPostersMovie = await fetch(url, options)
+        const postersData = await fetchingPostersMovie.json()
+
+        if (postersData && postersData.posters && postersData.posters.length > 0) {
+            const posterPath = `https://image.tmdb.org/t/p/original${postersData.posters[0].file_path}`;
+            postersPaths.push(posterPath)
+        } else {
+            console.log("erro ao obter imagens");
+        }
+    }
+    return postersPaths
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
-var cont = 1
 let isClickingTimeout = false;
 let isClickManual = false;
+var cont = 1
 var idRadioManualClicked = 0
+const sliderContent = document.getElementById('content');
+const slideBoxes = sliderContent.querySelectorAll('.slide-box');
 document.getElementById('radio1').checked = true
 
+// Set do intervalo para ir para proxima imagem do carousel
 setInterval(() => {
     nextImage()
-}, 3000)
-
+}, 5000)
+// Manipulação para ir para a proxima imagem do carousel, configurando o radiobutton correspondente a imagem
 function nextImage() {
     cont++
     if (cont > 3) {
@@ -16,7 +19,10 @@ function nextImage() {
     isClickManual = false
     document.getElementById('radio' + cont).checked = true
 }
-
+/*
+    Manipulação dos eventos de clique nos radio buttons;
+    Configura um timeout para limitar o clique continuo;
+*/
 document.addEventListener("DOMContentLoaded", function () {
     let radioButtons = document.querySelectorAll('input[type="radio"]');
     for (let buttonManual of radioButtons) {
@@ -33,20 +39,37 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
-
-function radioButtonManual(radioButtons) {
+/*
+    Configuração dos radio buttons;
+    Remove o 'active' de todas as labels de radio button quando é realizado o clique
+*/
+function radioButtonManual() {
     function changeRadioButton(idRadioManualClicked) {
-        // Remove a classe "active" de todos os labels
         document.querySelectorAll('.manual-btn').forEach(function (label) {
             label.classList.remove('active');
         });
         cont = idRadioManualClicked
     }
-
-    // Inicia o processo
     changeRadioButton(idRadioManualClicked);
 }
 
 function extractNumericPart(id) {
     return id.replace('radio', '');
 }
+// Adiciona novos <img> a <div> para aparecer a imagem do poster no carousel
+function imgSlideBox(postersNowPlayingMovie) {
+    for (let i = 0; i < postersNowPlayingMovie.length; i++) {
+        var slideBox = slideBoxes[i]
+        const img = document.createElement('img')
+        img.src = `${postersNowPlayingMovie[i]}`
+        img.alt = `Poster do filme de numero ${i}`
+        slideBox.appendChild(img)
+    }
+}
+// Manipula os dados assincronos obtidos da API
+(async function () {
+    const nowPlayingData = await fetchNowPlaying()
+    imgSlideBox(nowPlayingData)
+})()
+
+
