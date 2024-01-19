927efb8cf72358ede3af0b2e1c9351ff70a43a3b
matheusfinamor1 <matheusfinamor1@gmail.com>
2024-01-19 13:58:42 -0300

    adição do input de pesquisa

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,16 +1,21 @@
-const search = document.getElementById('imgSearch')
+const searchImg = document.getElementById('imgSearch')
+const contentInputSearch = document.getElementById('content-input-search')
 
 handlerSrcSearch();
 
 // Altera entre os icones de pesquisar/fechar ao clica-los no header
+// oculta e torna visible o input de pesquisa
+
 function handlerSrcSearch() {
-    if (search) {
-        search.addEventListener('click', function () {
-            console.log("clicou");
-            if (search.src.match("search.svg")) {
-                search.src = "../assets/img/close.svg";
+    if (searchImg && contentInputSearch) {
+        searchImg.addEventListener('click', function () {
+
+            if (searchImg.src.match("search.svg")) {
+                searchImg.src = "../assets/img/close.svg";
+                contentInputSearch.style.display = 'block'
             } else {
-                search.src = "../assets/img/search.svg";
+                searchImg.src = "../assets/img/search.svg";
+                contentInputSearch.style.display = 'none'
             }
         });
     }
