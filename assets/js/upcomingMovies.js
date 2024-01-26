a9fdd0067693cec5ea47365cb65cfc486f16650d
matheusfinamor1 <matheusfinamor1@gmail.com>
2024-01-26 12:15:42 -0300

    funcionalidade nos cliques  nos botoes "prev" e "next" do slider upcoming-movies

--- assets/js/upcomingMovies.js
+++ assets/js/upcomingMovies.js
@@ -1,6 +1,33 @@
 const sliderUpcomingContent = document.getElementById('content-upcoming')
 const slideBoxesUpcoming = sliderUpcomingContent.querySelectorAll('.slide-box-upcoming')
 
+// Manipula os eventos de clique nos botoes de "prev" e "next" image do slider
+document.addEventListener('DOMContentLoaded', function(){
+    var nextButton = document.getElementById('next-slider')
+    var prevButton = document.getElementById('prev-slider')
+    var sliderContent = document.getElementById('content-upcoming')
+    var image = document.querySelector(".content-imgs-upcoming span.slide-box-upcoming");
+
+    nextButton.addEventListener('click', function(){
+        scrollSlider('next')
+    })
+    prevButton.addEventListener('click', function(){
+        scrollSlider('prev')
+    })
+
+    function scrollSlider(direction){
+        if(image){
+            var widthImage = image.offsetWidth
+            var scrollAmount = widthImage
+            if(direction === 'next'){
+                sliderContent.scrollLeft += scrollAmount
+            }else if(direction === 'prev'){
+                sliderContent.scrollLeft -= scrollAmount
+            }
+        }
+    }
+})
+
 // Adiciona novos <img> a <span> para aparecer a imagem do poster no slider de files populares
 function imgSlideBoxUpcoming(postersUpcoming) {
     for (let i = 0; i < postersUpcoming.length; i++) {
@@ -17,3 +44,4 @@ function imgSlideBoxUpcoming(postersUpcoming) {
     upcomingMoviesData = await fetchIDUpcomingMovies()
     imgSlideBoxUpcoming(upcomingMoviesData)
 })()
+// window.addEventListener("load", initSlider)
